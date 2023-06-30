759f796d20a89468faa7fde72e100e6abace5d7b
tanaka <tanaka.for.public@gmail.com>
2023-06-30 22:18:17 +0900

    erased failed slice and double quatation

--- memo.rb
+++ memo.rb
@@ -1,17 +1,17 @@
-require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+require "csv"
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
 
-memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
+memo_type = gets.to_i
 
 if memo_type == 1
   puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
 	file_name = gets.chomp
 
   puts "メモ内容を記載してください。Ctrl+Dで保存します。"
-	memo = STDIN.read[1:-1]
+	memo = STDIN.read
 
-	CSV.open("#{file_name}.csv", "w") do |csv|
+	CSV.open("#{file_name}.csv", "w", quote_char: "") do |csv| # quote_charオプションを追加
   	csv << [memo]
 end
 
@@ -21,14 +21,14 @@ elsif memo_type == 2
 	
 	puts "メモしたい内容を入力して下さい。"
 	puts "完了したらCtrl+Dをおします。"
-	memo = STDIN.read[1:-1]
-     
-  CSV.open("#{file_name}.csv", "a") do |csv|
+	memo = STDIN.read
+  
+  CSV.open("#{file_name}.csv", "a", quote_char: "") do |csv| # quote_charオプションを追加
   	csv << [memo]
 end
 
 else
 
-	puts "1か2を入力してください。"
+	puts "1か2を入力してください."
     
 end
